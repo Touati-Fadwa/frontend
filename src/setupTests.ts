9c55f9648edddea2b3651ad08afb29fba772c8f0
Fadwa Touati <fadwatouati58@gmail.com>
2025-04-21 21:04:03 +0100

    first commit

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,20 @@
 
-import '@testing-library/jest-dom';
+// setupTests.ts
+import { vi } from 'vitest';
+
+// Simuler matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false, // Par défaut, vous pouvez ajuster selon le test
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // obsolète, mais parfois utilisé par les libs
+    removeListener: vi.fn(), // obsolète, mais parfois utilisé par les libs
+    addEventListener: vi.fn(), // Utilisé pour les événements
+    removeEventListener: vi.fn(), // Utilisé pour les événements
+    dispatchEvent: vi.fn(), // Permet de déclencher des événements
+  }),
+});
+
+  
